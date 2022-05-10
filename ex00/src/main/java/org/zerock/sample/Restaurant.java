74b8a854733370f422ed9ab082d35a6fa82b4585
apem5186 <apem5186@naver.com>
2022-05-10 21:41:23 +0900

    ##

---
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -0,0 +1,19 @@
+package org.zerock.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+//Setter 주입
+//생성자 주입
+//필드 주입 Autowired
+
+@Component
+@ToString
+@RequiredArgsConstructor
+public class Restaurant {
+
+	private final Chef chef;
+}
